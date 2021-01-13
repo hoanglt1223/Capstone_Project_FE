80b622e579dbb86be2d18f301ffc610bdb3ebba4
Đức Trung <ductrung4263@gmail.com>
2021-01-13 22:55:59 +0700

    CAPS-10 | Create UI SearchBar and Toolbar
    Create UI SearchBar and Toolbar

--- src/components/MainPage/components/ToolBars/index.tsx
+++ src/components/MainPage/components/ToolBars/index.tsx
@@ -4,27 +4,31 @@ import Button from '../../../Button'
 const ToolsBar = () => {
   return (
     <div className={styles.container}>
-      <button className={styles.button} type="submit">
-        New
-      </button>
-      <button className={styles.button} type="submit">
-        Copy
-      </button>
-      <button className={styles.button} type="submit">
-        Cut
-      </button>
-      <button className={styles.button} type="submit">
-        Delete
-      </button>
-      <button className={styles.button} type="submit">
-        Save
-      </button>
-      <button className={styles.button} type="submit">
-        Paste
-      </button>
-      <button className={styles.buttonExport} type="submit">
-        Export
-      </button>
+      <div className={styles.containerButton1}>
+        <button className={styles.button} type="submit">
+          New
+        </button>
+        <button className={styles.button} type="submit">
+          Copy
+        </button>
+        <button className={styles.button} type="submit">
+          Cut
+        </button>
+        <button className={styles.button} type="submit">
+          Delete
+        </button>
+        <button className={styles.button} type="submit">
+          Save
+        </button>
+        <button className={styles.button} type="submit">
+          Paste
+        </button>
+      </div>
+      <div className={styles.containerButton2}>
+        <button className={styles.buttonExport} type="submit">
+          Export
+        </button>
+      </div>
       <h2 className={styles.text}>KIXBUILDER</h2>
     </div>
   )
