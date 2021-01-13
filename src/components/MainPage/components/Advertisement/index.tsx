749f1f444593aae9a1c6f2fb6c1b13f9d5d43462
Dung Truong <zeket412@gmail.com>
2021-01-13 21:40:29 +0700

    CAPS-11 | add image

--- src/components/MainPage/components/Advertisement/index.tsx
+++ src/components/MainPage/components/Advertisement/index.tsx
@@ -4,7 +4,9 @@ import styles from './advertisement.module.scss'
 const Advertisement = () => {
   return (
     <div className={styles.container}>
-      <div>Advertisement</div>
+      <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/7_Up_-_You_like_it%2C_it_likes_you%2C_1948.jpg/1200px-7_Up_-_You_like_it%2C_it_likes_you%2C_1948.jpg" alt="example" />
+      </div>
     </div>
   )
 }
