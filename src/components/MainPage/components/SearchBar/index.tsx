c8c988f81cbc0d2788977ac16de06a0f3813186d
Thanh Hoàng <fightforml@gmail.com>
2021-01-17 00:55:50 +0700

    fix style grid, update search placeholder

--- src/components/MainPage/components/SearchBar/index.tsx
+++ src/components/MainPage/components/SearchBar/index.tsx
@@ -4,7 +4,7 @@ import styles from './searchBar.module.scss'
 const SearchBar = () => {
   return (
     <div className={styles.container}>
-      <input className={styles.inputSearch} type="text" placeholder="Enter what you want to search"></input>
+      <input className={styles.inputSearch} type="text" placeholder="Enter your keyword..."></input>
       <div className={styles.containerScreen}>
         <button className={styles.buttonScreen1}>Sign Up</button>
         <button className={styles.buttonScreen1}>Sign In</button>
