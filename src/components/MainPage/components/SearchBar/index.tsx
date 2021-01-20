3fe20ce7532c6f79cf79adeaddcf3f047af11687
Thanh Hoàng <fightforml@gmail.com>
2021-01-20 21:22:12 +0700

    log out

--- src/components/MainPage/components/SearchBar/index.tsx
+++ src/components/MainPage/components/SearchBar/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import routes from '../../../../routes'
 import styles from './searchBar.module.scss'
 
 const SearchBar = () => {
+  const history = useHistory()
+  function logout() {
+    localStorage.setItem('userToken', '')
+    history.push(routes.value)
+  }
   return (
     <div className={styles.container}>
       <input className={styles.inputSearch} type="text" placeholder="Enter your keyword..."></input>
@@ -10,7 +17,7 @@ const SearchBar = () => {
         <button className={styles.buttonScreen1}>Sign In</button>
       </div>
       <div className={styles.containerLogout}>
-        <button onClick={() => localStorage.setItem('userToken', '')} className={styles.buttonLogout}>
+        <button onClick={() => logout()} className={styles.buttonLogout}>
           Log out
         </button>
       </div>
